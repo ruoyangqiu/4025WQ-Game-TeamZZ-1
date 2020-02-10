7d6e70a56174f2526f9a4c1039907bd9af16f79d
Tianhua Zhao <zhaot2@seattleu.edu>
2020-02-09 23:21:40 -0800

    Added Default Character Data

--- Game/Game/Services/DefaultData.cs
+++ Game/Game/Services/DefaultData.cs
@@ -64,5 +64,42 @@ namespace Game.Services
 
             return datalist;
         }
+
+        /// <summary>
+        /// Load the Default character data
+        /// </summary>
+        /// <returns></returns>
+        public static List<CharacterModel> LoadData(CharacterModel temp)
+        {
+            var datalist = new List<CharacterModel>()
+            {
+                new CharacterModel {
+                    Name = "Sun Wukong",
+                    Description = "Sun Wukong is a skilled fighter, capable of " +
+                    "defeating the best warriors of heaven. His hair possesses " +
+                    "magical properties, capable of summoning clones of the Monkey King " +
+                    "himself, and/or into various weapons, animals, and other objects.",
+                    ImageURI = "http://www.clker.com/cliparts/e/L/A/m/I/c/sword-md.png" },
+
+                new CharacterModel {
+                    Name = "Zhu Bajie",
+                    Description = "Zhu Bajie looks like a terrible monster, part human and part pig, " +
+                    "who often gets himself and his companions into trouble through his laziness, " +
+                    "gluttony, and propensity for lusting after pretty women. He is jealous of " +
+                    "Sun Wukong and always tries to bring him down.",
+                    ImageURI = "http://www.clipartbest.com/cliparts/4T9/LaR/4T9LaReTE.png",},
+
+                new CharacterModel {
+                    Name = "Tang Sanzang",
+                    Description = "Tang Sanzang was the founder of the Idealistic School. " +
+                    "He translated abundant Buddhist canons and possessed an important position " +
+                    "in Buddhist history. Xuanzang became a monk at the age of 12 and became a monk " +
+                    "at the age of 21.",
+                    ImageURI = "http://www.clipartbest.com/cliparts/yik/e9k/yike9kMyT.png",},
+            };
+
+            return datalist;
+        }
+
     }
 }
