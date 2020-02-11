0ff423e3d3f92d5bb9f5f4e11c2f24eaae0ee742
Tianhua Zhao <zhaot2@seattleu.edu>
2020-02-10 17:42:06 -0800

    Added more entries to the Character URI and Descriptions list

--- Game/Game/Services/CharacterService.cs
+++ Game/Game/Services/CharacterService.cs
@@ -15,6 +15,11 @@ namespace Game.Services
             "https://i.ibb.co/nfVF1qg/01543.png",
             "https://i.ibb.co/37J2j7m/01545.png",
             "https://i.ibb.co/KG3KFbp/01546.png",
+            "https://i.ibb.co/5kmbn1h/01548.png",
+            "https://i.ibb.co/FHcf1wF/01550.png",
+            "https://i.ibb.co/VQDNHyY/01554.png",
+            "https://i.ibb.co/HVKhVg3/01555.png"
+
         };
 
         // A predefined list for character descriptions. When a character gets created, a random description from this list is assigned to the character.
@@ -22,7 +27,11 @@ namespace Game.Services
         {
             "Incarnation of the horse god",
             "Incarnation of the pig god",
-            "Incarnation of the rabbit god.",
+            "Incarnation of the rabbit god",
+            "Incarnation of the dragon god",
+            "Incarnation of the snake god",
+            "Incarnation of the goat god",
+            "Incarnation of the monkey god",
         };
 
         /// <summary>
