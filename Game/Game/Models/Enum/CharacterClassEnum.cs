834ee49ad086bef9101969cb6945d8ca8a47c207
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-02-12 22:08:09 -0800

    Added Comments to CharacterClassEnum

--- Game/Game/Models/Enum/CharacterClassEnum.cs
+++ Game/Game/Models/Enum/CharacterClassEnum.cs
@@ -7,7 +7,11 @@ namespace Game.Models
     public enum CharacterClassEnum
     {
         Unknown = 0,
+
+        // Fighte has highier Attack when created
         Fighter = 10,
+
+        // Cleric has highier Defence when created
         Cleric = 12,
     }
 }
