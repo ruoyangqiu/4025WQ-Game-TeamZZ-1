2e781b169a9219e57b8c44be11ae92583bd16748
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-02-12 21:45:45 -0800

    Added DifficultyLevelEnum

---
+++ Game/Game/Models/Enum/DifficultyLevelEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models.Enum
+{
+    public enum DifficultyLevelEnum
+    {
+        Easy = 10,
+
+        Medium = 12,
+
+        Hard = 14,
+    }
+}
