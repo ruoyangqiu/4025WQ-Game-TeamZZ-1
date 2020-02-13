238822c0e13926cd1af9c99de7683563241f7b7e
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-02-12 22:00:22 -0800

    Add comments to DifficultyLevelEnum

--- Game/Game/Models/Enum/DifficultyLevelEnum.cs
+++ Game/Game/Models/Enum/DifficultyLevelEnum.cs
@@ -6,10 +6,13 @@ namespace Game.Models.Enum
 {
     public enum DifficultyLevelEnum
     {
+        // The easiest level of Monster, easy to kill, low damage to character
         Easy = 10,
 
+        // Normal level Monster, will take a while to kill, medium damage 
         Medium = 12,
 
+        // Usually the final boss of a round, hard to kill, high damage
         Hard = 14,
     }
 }
