914bbd171eb0471b2e987f39e28bd81df87d83b9
Tianhua Zhao <zhaot2@seattleu.edu>
2020-02-12 21:11:11 -0800

    Added Monsters to HomeMenuItemModel list.

--- Game/Game/Views/MenuPage.xaml.cs
+++ Game/Game/Views/MenuPage.xaml.cs
@@ -33,6 +33,7 @@ namespace Game.Views
                 new HomeMenuItemModel {Id = MenuItemEnum.Village, Title="Village" },
                 new HomeMenuItemModel {Id = MenuItemEnum.Battle, Title="Battle" },
                 new HomeMenuItemModel {Id = MenuItemEnum.Characters, Title="Characters" },
+                new HomeMenuItemModel {Id = MenuItemEnum.Monsters, Title="Monsters" },
                 new HomeMenuItemModel {Id = MenuItemEnum.Items, Title="Items" },
                 new HomeMenuItemModel {Id = MenuItemEnum.Score, Title="Score" },
             };
