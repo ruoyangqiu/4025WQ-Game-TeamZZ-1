13866c1bc97ab4fe724fbab1bf5f00096d36cf4a
Tianhua Zhao <zhaot2@seattleu.edu>
2020-02-12 20:43:06 -0800

    Created MonsterModel, partial submit. Todo: Add all properties.

---
+++ Game/Game/Models/MonsterModel.cs
@@ -0,0 +1,53 @@
+﻿using Game.Services;
+
+namespace Game.Models
+{
+    /// <summary>
+    /// Character for the Game
+    /// </summary>
+    public class MonsterModel : BaseModel<MonsterModel>
+    {
+
+        /// <summary>
+        /// Default MonsterModel
+        /// Establish the Default Image Path
+        /// </summary>
+        public MonsterModel() {
+            Name = "";
+            ImageURI = CharacterService.DefaultImageURI;
+        }
+
+        /// <summary>
+        /// Constructor to create a Monster based on what is passed in
+        /// </summary>
+        /// <param name="data"></param>
+        public MonsterModel(MonsterModel data)
+        {
+            Update(data);
+            
+        }
+
+        /// <summary>
+        /// Update the Record
+        /// </summary>
+        /// <param name="newData">The new data</param>
+        public override void Update(MonsterModel newData)
+        {
+            if (newData == null)
+            {
+                return;
+            }
+
+            // Update all the fields in the Data, except for the Id and guid
+            Name = newData.Name;
+        }
+
+        // Helper to combine the attributes into a single line, to make it easier to display the character as a string
+        public string FormatOutput()
+        {
+            var myReturn = Name;
+
+            return myReturn.Trim();
+        }
+    }
+}
