bdacd0e6e00d57d8c9299449a759b5a426aa8bb7
Tianhua Zhao <zhaot2@seattleu.edu>
2020-02-13 22:18:42 -0800

    Added MonsterDeletePage

---
+++ Game/Game/Views/Monsters/MonsterDeletePage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Game.ViewModels;
+using System;
+using Game.Models;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// The Read Page
+    /// </summary>
+    [DesignTimeVisible(false)]
+    public partial class MonsterDeletePage : ContentPage
+    {
+        // View Model for Monster
+        readonly GenericViewModel<MonsterModel> viewModel;
+
+        // Constructor for Delete takes a view model of what to delete
+        public MonsterDeletePage(GenericViewModel<MonsterModel> data)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = data;
+
+            this.viewModel.Title = "Delete " + data.Title;
+        }
+
+        /// <summary>
+        /// Save calls to Update
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async void Delete_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send(this, "Delete", viewModel.Data);
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Cancel and close this page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Trap the Back Button on the Phone
+        /// </summary>
+        /// <returns></returns>
+        protected override bool OnBackButtonPressed()
+        {
+            // Add your code here...
+            return true;
+        }
+    }
+}
