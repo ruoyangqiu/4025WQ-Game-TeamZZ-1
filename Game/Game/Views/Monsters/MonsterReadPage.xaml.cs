98b22c60a0b4148444f263bad98945eb6eb16797
Tejas Dandane <tejudandane@gamil.com>
2020-02-13 16:28:39 -0800

    added monster details in the MonsterReadPage

--- Game/Game/Views/Monsters/MonsterReadPage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterReadPage.xaml.cs
@@ -4,17 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Game.Models;
+using Game.ViewModels;
 
-namespace Game.Views.Monsters
+namespace Game.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MonsterReadPage : ContentPage
     {
-        public MonsterReadPage()
+        readonly GenericViewModel<MonsterModel> ViewModel;
+
+        /// <summary>
+        /// Constructor called with a view model
+        /// This is the primary way to open the page
+        /// The viewModel is the data that should be displayed
+        /// </summary>
+        /// <param name="viewModel"></param>
+        public MonsterReadPage(GenericViewModel<MonsterModel> data)
         {
             InitializeComponent();
+
+            BindingContext = this.ViewModel = data;
         }
     }
 }
