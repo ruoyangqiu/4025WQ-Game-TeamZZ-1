4f3dbf7dbdfa9aff786f7e92d840ebbb16d12cc6
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-02-18 09:28:13 -0800

    Added Comments to DifficultyLevelEnumHelper

--- Game/Game/Helpers/DifficultyLevelEnumHelper.cs
+++ Game/Game/Helpers/DifficultyLevelEnumHelper.cs
@@ -8,6 +8,10 @@ namespace Game.Helpers
 {
     static class DifficultyLevelEnumHelper
     {
+        /// <summary>
+        /// Returns a list of strings of the enum for Difficulty level
+        /// Removes the unknown
+        /// </summary>
         public static List<string> GetListDifficulty
         {
             get
@@ -16,7 +20,11 @@ namespace Game.Helpers
                 return myList;
             }
         }
-
+        /// <summary>
+        /// Given the String for an enum, return its value.  That allows for the enums to be numbered 2,4,6 rather than 1,2,3
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public static DifficultyLevelEnum ConvertStringToEnum(string value)
         {
             return (DifficultyLevelEnum)Enum.Parse(typeof(DifficultyLevelEnum), value);
