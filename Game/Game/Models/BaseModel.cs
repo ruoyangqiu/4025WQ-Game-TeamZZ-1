1758b67af86dddce41d84736c58ccd26c3180829
Tianhua Zhao <zhaot2@seattleu.edu>
2020-02-19 01:08:33 -0800

    Added Comment for Update method in BaseModel. Added default ImageURI value

--- Game/Game/Models/BaseModel.cs
+++ Game/Game/Models/BaseModel.cs
@@ -9,8 +9,13 @@ namespace Game.Models
     {
 
         // Location to the image for the item.  Will come from the server as a fully qualified URI example:  https://developer.android.com/images/robot-tiny.png
-        public string ImageURI { get; set; }
+        public string ImageURI { get; set; } = Services.ItemService.DefaultImageURI;
 
+        /// <summary>
+        /// Update Method is virutal and changed for each class
+        /// </summary>
+        /// <param name="newData"></param>
+        /// <returns></returns>
         public virtual void Update(T newData)
         {
 
