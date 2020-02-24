dcb51230c425a1864d9aee37b0bec074ee704283
Tejas Dandane <tejudandane@gamil.com>
2020-02-24 15:19:44 -0800

    Added new RandomPlayerHelper

---
+++ Game/Game/Helpers/RandomPlayerHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Game.Models;
+using Game.ViewModels;
+using System.Linq;
+
+namespace Game.Helpers
+{
+    public static class RandomPlayerHelper
+    {
+        /// <summary>
+        /// Get Health
+        /// </summary>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        public static int GetHealth(int level)
+        {
+            // Roll the Dice and reset the Health
+            return DiceHelper.RollDice(level, 10);
+        }
+    }
+}
