57c924ff4e0aa5da7d34797c46bc9a000169370c
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-02-23 21:09:34 -0800

    Created BasePropertyDetailsModel

---
+++ Game/Game/Models/BasePropertyDetailsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models
+{
+    /// <summary>
+    /// Hold the details of base properties for each class of Character 
+    /// and each difficulty of Monster
+    /// </summary>
+    class BasePropertyDetailsModel
+    {
+    }
+}
