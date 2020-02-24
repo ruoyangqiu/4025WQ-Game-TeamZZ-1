832f71942cbcd1d8e01085c337762b1d9ebf9e57
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-02-23 21:28:25 -0800

    Added BasePropertyHelper to define base attribute for monster and character

---
+++ Game/Game/Helpers/BasePropertyHelper.cs
@@ -0,0 +1,69 @@
+﻿using Game.Models;
+using Game.Models.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Helpers
+{
+    /// <summary>
+    /// Helper to manage BaseProperrty data
+    /// </summary>
+    class BasePropertyHelper
+    {
+        #region Singleton
+
+        private static BasePropertyHelper _instance;
+
+        public static BasePropertyHelper Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new BasePropertyHelper();
+                }
+                return _instance;
+            }
+        }
+
+        #endregion Singleton
+
+        // Dictionary to store the Monster's base properties based on difficulty level
+        public Dictionary<DifficultyLevelEnum, BasePropertyDetailsModel> MonsterDifficultyBase;
+
+        // Dictionary to store the Character's base properties based on their class
+        public Dictionary<CharacterClassEnum, BasePropertyDetailsModel> CharacterClassBase;
+
+        /// <summary>
+        /// Constructor calls to clear the data
+        /// </summary>
+        public BasePropertyHelper()
+        {
+            ClearAndLoadDataTable();
+        }
+
+        /// <summary>
+        /// Clear the data and reload it
+        /// </summary>
+        public void ClearAndLoadDataTable()
+        {
+            MonsterDifficultyBase = new Dictionary<DifficultyLevelEnum, BasePropertyDetailsModel>();
+            CharacterClassBase = new Dictionary<CharacterClassEnum, BasePropertyDetailsModel>();
+            LoadBasePropertyData();
+        }
+
+        /// <summary>
+        /// Load data for each difficulty and class
+        /// </summary>
+        public void LoadBasePropertyData()
+        {
+            MonsterDifficultyBase.Add(DifficultyLevelEnum.Easy, new BasePropertyDetailsModel(1, 1, 1, 5));
+            MonsterDifficultyBase.Add(DifficultyLevelEnum.Medium, new BasePropertyDetailsModel(3, 3, 3, 10));
+            MonsterDifficultyBase.Add(DifficultyLevelEnum.Hard, new BasePropertyDetailsModel(5, 5, 5, 15));
+
+            CharacterClassBase.Add(CharacterClassEnum.Fighter, new BasePropertyDetailsModel(3, 1, 1, 5));
+            CharacterClassBase.Add(CharacterClassEnum.Cleric, new BasePropertyDetailsModel(1, 3, 1, 5));
+        }
+    }
+}
