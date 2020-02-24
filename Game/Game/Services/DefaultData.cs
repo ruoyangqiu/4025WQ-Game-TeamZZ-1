a2d23b0e1cf8fe32885c79185eec935a7a596553
Tianhua Zhao <zhaot2@seattleu.edu>
2020-02-23 21:51:30 -0800

    Removed Default Items from Mike's base project.

--- Game/Game/Services/DefaultData.cs
+++ Game/Game/Services/DefaultData.cs
@@ -13,35 +13,6 @@ namespace Game.Services
         {
             var datalist = new List<ItemModel>()
             {
-                new ItemModel {
-                    Name = "Gold Sword",
-                    Description = "Sword made of Gold, really expensive looking",
-                    ImageURI = "http://www.clker.com/cliparts/e/L/A/m/I/c/sword-md.png",
-                    Range = 0,
-                    Damage = 9,
-                    Value = 9,
-                    Location = ItemLocationEnum.PrimaryHand,
-                    Attribute = AttributeEnum.Attack},
-
-                new ItemModel {
-                    Name = "Strong Shield",
-                    Description = "Enough to hide behind",
-                    ImageURI = "http://www.clipartbest.com/cliparts/4T9/LaR/4T9LaReTE.png",
-                    Range = 0,
-                    Damage = 0,
-                    Value = 9,
-                    Location = ItemLocationEnum.OffHand,
-                    Attribute = AttributeEnum.Defense},
-
-                new ItemModel {
-                    Name = "Bunny Hat",
-                    Description = "Pink hat with fluffy ears",
-                    ImageURI = "http://www.clipartbest.com/cliparts/yik/e9k/yike9kMyT.png",
-                    Range = 0,
-                    Damage = 0,
-                    Value = 9,
-                    Location = ItemLocationEnum.Head,
-                    Attribute = AttributeEnum.Speed},
 
                 new ItemModel {
                     Name = "Blue Horn",
