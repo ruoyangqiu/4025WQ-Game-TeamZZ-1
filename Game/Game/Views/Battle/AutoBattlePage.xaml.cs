95c27523d3706a3893c90ba2841a3a12cd3f5b92
Tianhua Zhao <zhaot2@seattleu.edu>
2020-02-24 21:23:10 -0800

    Updated Mock BattleMessage string on AutoBattlePage.

--- Game/Game/Views/Battle/AutoBattlePage.xaml.cs
+++ Game/Game/Views/Battle/AutoBattlePage.xaml.cs
@@ -20,7 +20,19 @@ namespace Game.Views
 
 		public async void AutobattleButton_Clicked(object sender, EventArgs e)
 		{
-			await Navigation.PushModalAsync(new ScorePage());
+			// Call into Auto Battle from here to do the Battle...
+
+			//var Engine = new Game.Engine.AutoBattleEngine();
+
+			//string BattleMessage = "";
+
+			//var result = await Engine.RunAutoBattle();
+
+			//var Score = Engine.GetScoreObject();
+
+			string BattleMessage = string.Format("Number of Rounds： 7 \n Monsters Killed： 19\n EXP Gained：9500\n Items Dropped:\n Blue Horn X 1, Golden Hair Pin X 3");
+
+			BattleMessageValue.Text = BattleMessage;
 		}
 	}
 }
