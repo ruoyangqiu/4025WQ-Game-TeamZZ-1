afa250b0faa4a54c5d494154290a071501f0a8bb
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-02-25 13:28:50 -0800

    Added Constructor to EntityInfoModel

--- Game/Game/Models/EntityInfoModel.cs
+++ Game/Game/Models/EntityInfoModel.cs
@@ -4,7 +4,126 @@ using System.Text;
 
 namespace Game.Models
 {
-    class EntityInfoModel
+    /// <summary>
+    /// Player for the game.
+    /// 
+    /// Either Monster or Character
+    /// 
+    /// Constructor Player to Player used a T in Round
+    /// </summary>
+    public class PlayerInfoModel : EntityModel<PlayerInfoModel>
     {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public PlayerInfoModel() { }
+
+        /// <summary>
+        /// Copy from one PlayerInfoModel into Another
+        /// </summary>
+        /// <param name="data"></param>
+        public PlayerInfoModel(PlayerInfoModel data)
+        {
+            PlayerType = data.PlayerType;
+            Guid = data.Guid;
+            Alive = data.Alive;
+            Experience = data.Experience;
+            //ExperienceRemaining = data.ExperienceRemaining;
+            Level = data.Level;
+            Name = data.Name;
+            Speed = data.GetSpeed();
+            ImageURI = data.ImageURI;
+            CurrentHealth = data.GetCurrentHealth();
+            MaxHealth = data.GetMaxHealth();
+
+            // Set the strings for the items
+            Head = data.Head;
+            Feet = data.Feet;
+            Necklace = data.Necklace;
+            RightFinger = data.RightFinger;
+            LeftFinger = data.LeftFinger;
+            Feet = data.Feet;
+
+        }
+
+        /// <summary>
+        /// Create PlayerInfoModel from character
+        /// </summary>
+        /// <param name="data"></param>
+        public PlayerInfoModel(CharacterModel data)
+        {
+            PlayerType = data.PlayerType;
+            Guid = data.Guid;
+            Alive = data.Alive;
+            Experience = data.Experience;
+            //ExperienceRemaining = data.ExperienceRemaining;
+            Level = data.Level;
+            Name = data.Name;
+            Description = data.Description;
+            Speed = data.GetSpeed();
+            ImageURI = data.ImageURI;
+            CurrentHealth = data.GetCurrentHealth();
+            MaxHealth = data.GetMaxHealth();
+
+            // Set the strings for the items
+            Head = data.Head;
+            Feet = data.Feet;
+            Necklace = data.Necklace;
+            RightFinger = data.RightFinger;
+            LeftFinger = data.LeftFinger;
+            Feet = data.Feet;
+
+        }
+
+        /// <summary>
+        /// Crate PlayerInfoModel from Monster
+        /// </summary>
+        /// <param name="data"></param>
+        public PlayerInfoModel(MonsterModel data)
+        {
+            PlayerType = data.PlayerType;
+            Guid = data.Guid;
+            Alive = data.Alive;
+            Experience = data.Experience;
+            //ExperienceRemaining = data.ExperienceRemaining;
+            Level = data.Level;
+            Name = data.Name;
+            Description = data.Description;
+            Speed = data.GetSpeed();
+            ImageURI = data.ImageURI;
+            CurrentHealth = data.GetCurrentHealth();
+            MaxHealth = data.GetMaxHealth();
+
+            // Set the strings for the items
+            Head = data.Head;
+            Feet = data.Feet;
+            Necklace = data.Necklace;
+            RightFinger = data.RightFinger;
+            LeftFinger = data.LeftFinger;
+            Feet = data.Feet;
+
+            //DifficultyLevel = data.DifficultyLevel;
+        }
+
+        //public override string FormatOutput()
+        //{
+        //    var myReturn = string.Empty;
+        //    myReturn += Name;
+        //    myReturn += " , " + Description;
+        //    myReturn += " , Level : " + Level.ToString();
+
+        //    if (PlayerType == PlayerTypeEnum.Character)
+        //    {
+        //        myReturn += " , Total Experience : " + Experience;
+        //        myReturn += " , Damage : " + GetDamageTotalString;
+        //        myReturn += " , Attack :" + GetAttackTotal;
+        //        myReturn += " , Defense :" + GetDefenseTotal;
+        //        myReturn += " , Speed :" + GetSpeedTotal;
+        //    }
+
+        //    myReturn += " , Items : " + ItemSlotsFormatOutput();
+
+        //    return myReturn;
+        //}
     }
 }
