72fb2426b8934d4e4a5b96bc35183240b38a72e2
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-03-01 12:07:32 -0800

    Added attributes to BattleMessageModel

--- Game/Game/Models/BattleMessageModel.cs
+++ Game/Game/Models/BattleMessageModel.cs
@@ -4,7 +4,51 @@ using System.Text;
 
 namespace Game.Models
 {
-    class BattleMessageModel
+    /// <summary>
+    /// Manage the message formatting for ui to display
+    /// </summary>
+    public class BattleMessageModel
     {
+        #region Message Properties
+        // Is the player a character or a monster
+        public PlayerTypeEnum PlayerType = PlayerTypeEnum.Unknown;
+
+        // The Status of the action
+        public HitStatusEnum HitStatus = HitStatusEnum.Unknown;
+
+        // Name of the Attacker
+        public string AttackerName = string.Empty;
+
+        // Name of who the target was
+        public string TargetName = string.Empty;
+
+        // The status of the Attack
+        public string AttackStatus = string.Empty;
+
+        // Turn Message
+        public string TurnMessage = string.Empty;
+
+        // Turn Special Message
+        public string TurnMessageSpecial = string.Empty;
+
+        // Turn Experience Earned Message
+        public string ExperienceEarned = string.Empty;
+
+        // Level Up Message
+        public string LevelUpMessage = string.Empty;
+
+        // Amount of Damage
+        public int DamageAmount = 0;
+
+        // The Remaining Health Mesage
+        public int CurrentHealth = 0;
+
+        // Beginning of the Html Block for html formatting
+        public string htmlHead = @"<html><body bgcolor=""#E8D0B6""><p>";
+
+        // Ending of the Html Block for Html formatting
+        public string htmlTail = @"</p></body></html>";
+
+        #endregion Message Properties
     }
 }
