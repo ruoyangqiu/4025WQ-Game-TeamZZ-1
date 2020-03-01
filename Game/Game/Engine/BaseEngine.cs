3a6b1c6faf427394b5d100e53d4737cfad445340
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-03-01 12:52:05 -0800

    Added Properties to BaseEngine

--- Game/Game/Engine/BaseEngine.cs
+++ Game/Game/Engine/BaseEngine.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Game.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,7 +11,44 @@ namespace Game.Engine
     class BaseEngine
     {
         #region Properties
+        // Holds the official ScoreModel
+        public ScoreModel BattleScore = new ScoreModel();
 
+        // Holds the Battle Messages as they happen
+        public BattleMessageModel BattleMessagesModel = new BattleMessageModel();
+
+        // The Pool of items collected during the round as turns happen
+        public List<ItemModel> ItemPool = new List<ItemModel>();
+
+        // List of Monsters
+        public List<EntityInfoModel> MonsterList = new List<EntityInfoModel>();
+
+        // List of Characters
+        public List<EntityInfoModel> CharacterList = new List<EntityInfoModel>();
+
+        // Current Player who is the attacker
+        public EntityInfoModel CurrentAttacker;
+
+        // Current Player who is the Defender
+        public EntityInfoModel CurrentDefender;
+
+        // Hold the list of players (MonsterModel, and character by guid), and order by speed
+        public List<EntityInfoModel> PlayerList = new List<EntityInfoModel>();
+
+        // Current Round State
+        public RoundEnum RoundStateEnum = RoundEnum.Unknown;
+
+        // Max Number of Characters
+        public int MaxNumberPartyCharacters = 6;
+
+        // Max Number of Monsters
+        public int MaxNumberPartyMonsters = 6;
+
+        // Max Number of Rounds for AutoBattle
+        public int MaxRoundCount = 1000;
+
+        // Max Number of Turns for AutoBattle
+        public int MaxTurnCount = 10000;
 
         #endregion Properties
     }
