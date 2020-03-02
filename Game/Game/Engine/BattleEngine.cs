02f288b0fd75e5d8a2fe84e6b1fa267da98cf7fe
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-03-01 17:34:12 -0800

    Update EndBattle in BattleEngine

--- Game/Game/Engine/BattleEngine.cs
+++ Game/Game/Engine/BattleEngine.cs
@@ -50,7 +50,7 @@ namespace Game.Engine
         {
             BattleRunning = false;
 
-            //BattleScore.CalculateScore();
+            BattleScore.CalculateScore();
 
             return true;
         }
