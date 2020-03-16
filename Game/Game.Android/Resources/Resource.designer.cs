2ea86b50f1cb6fa88fff3682972ad73a26564eca
Tianhua Zhao <tzhaojustdoit@gmail.com>
2020-03-15 19:55:16 -0700

    Added question mark image

--- Game/Game.Android/Resources/Resource.designer.cs
+++ Game/Game.Android/Resources/Resource.designer.cs
@@ -5906,31 +5906,34 @@ namespace Game.Droid
 			public const int phase_boots = 2131165338;
 			
 			// aapt resource value: 0x7F07009B
-			public const int red_horn = 2131165339;
+			public const int question_mark = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int score = 2131165340;
+			public const int red_horn = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int splashscreen = 2131165341;
+			public const int score = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int tooltip_frame_dark = 2131165342;
+			public const int splashscreen = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int tooltip_frame_light = 2131165343;
+			public const int tooltip_frame_dark = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int troll = 2131165344;
+			public const int tooltip_frame_light = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int village = 2131165345;
+			public const int troll = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int vsImage = 2131165346;
+			public const int village = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int xamarin_logo = 2131165347;
+			public const int vsImage = 2131165347;
+			
+			// aapt resource value: 0x7F0700A4
+			public const int xamarin_logo = 2131165348;
 			
 			static Drawable()
 			{
