cf0f8f1464c7d6b2a82c7f60905e1d4a232247d4
Tejas Dandane <tejudandane@gamil.com>
2020-03-19 23:29:11 -0700

    Added unit test to AttributeEnumHelper

---
+++ UnitTests/Helpers/ItemLocationEnumHelperTests.cs
@@ -0,0 +1,14 @@
+﻿using Game.Models;
+using Game.Helpers;
+using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace UnitTests.Helpers
+{
+    [NUnit.Framework.TestFixture]
+    class ItemLocationEnumHelperTests
+    {
+       
+    }
+}
