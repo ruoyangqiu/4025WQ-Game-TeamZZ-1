263bceea267886769902b3fd0a54c0180da4226a
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-03-19 21:09:06 -0700

    Added ParseJson to ItemModelJsonHelper

--- Game/Game/Helpers/ItemModelJsonHelper.cs
+++ Game/Game/Helpers/ItemModelJsonHelper.cs
@@ -1,10 +1,57 @@
-﻿using System;
+﻿using Game.Models;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace Game.Helpers
 {
-    class ItemModelJsonHelper
+    public static class ItemModelJsonHelper
     {
+        /// <summary>
+        /// ParseJson takes the raw stirng and parses it into valid ItemModel
+        /// 
+        /// The returned data will be a list of items.  Need to pull that list out
+        /// </summary>
+        /// <param name="myJsonData"></param>
+        /// <returns></returns>
+        public static List<ItemModel> ParseJson(string myJsonData)
+        {
+            var myData = new List<ItemModel>();
+
+            try
+            {
+                JObject json;
+                json = JObject.Parse(myJsonData);
+
+                // Data is a List of Items, so need to pull them out one by one...
+
+                var myTempList = json["ItemList"].ToObject<List<JObject>>();
+
+                foreach (var myItem in myTempList)
+                {
+                    var myTempObject = ConvertFromJson(myItem);
+                    if (myTempObject != null)
+                    {
+                        myData.Add(myTempObject);
+                    }
+                }
+
+                return myData;
+            }
+            catch (Exception Ex)
+            {
+                Debug.WriteLine(Ex.ToString());
+                return null;
+            }
+        }
+
+        public static ItemModel ConvertFromJson(JObject json)
+        {
+            var myData = new ItemModel();
+            return myData;
+        }
+
     }
 }
