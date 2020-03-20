882beab9bf8409e55bc4075c9174f6d752e716ad
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-03-19 17:37:43 -0700

    Added Instance to HttpClientService

--- Game/Game/Services/HttpClientService.cs
+++ Game/Game/Services/HttpClientService.cs
@@ -1,10 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 
 namespace Game.Services
 {
     public class HttpClientService
     {
+        // Make this a singleton so it only exist one time because holds all the data records in memory
+        private static HttpClientService _instance;
+
+        // Local Instance of the Client
+        private HttpClient _httpClientInstance;
+
+        // client
+        private HttpClient _httpClient
+        {
+            get
+            {
+                if (_httpClientInstance == null)
+                {
+                    _httpClientInstance = new HttpClient();
+                }
+                return _httpClientInstance;
+            }
+            //set
+            //{
+            //    _httpClientInstance = _httpClient;
+            //}
+        }
+
+        // this instance
+        public static HttpClientService Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new HttpClientService();
+                }
+                return _instance;
+            }
+        }
     }
 }
