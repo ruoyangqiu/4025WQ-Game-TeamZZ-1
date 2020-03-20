f327ec113662271f6f1d3bb10f69eca8b180eef0
Tianhua Zhao <tzhaojustdoit@gmail.com>
2020-03-19 23:11:44 -0700

    Added CharacterModel_Constructor_Default_Should_Pass

--- UnitTests/Models/CharacterModelTests.cs
+++ UnitTests/Models/CharacterModelTests.cs
@@ -1,10 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
+
+using Game.Models;
+using Game.ViewModels;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace UnitTests.Models
 {
-    class CharacterModelTests
+    [TestFixture]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "<Pending>")]
+    public class CharacterModelTests
     {
+        [TearDown]
+        public void TearDown()
+        {
+            // For Tear down delete the Item Dataset.
+            // Test that need the Item Dataset should set it
+            ItemIndexViewModel.Instance.Dataset.Clear();
+        }
+
+        [Test]
+        public void CharacterModel_Constructor_Default_Should_Pass()
+        {
+            // ArDefense
+
+            // Act
+            var result = new CharacterModel();
+
+            // Reset
+
+            // Assert 
+            Assert.IsNotNull(result);
+        }
     }
 }
