ddae55ef5e135ebdabb282e52346d5364eddc595
Tejas Dandane <tejudandane@gamil.com>
2020-03-19 23:47:01 -0700

    Added unit test IntEnumConvert_Should_Skip in IntEnumConverterHelperTests

---
+++ UnitTests/Helpers/IntEnumConverterHelperTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Game.Models;
+using Game.Helpers;
+
+namespace UnitTests.Helpers
+{
+    [TestFixture]
+    class IntEnumConverterHelperTests
+    {
+        [Test]
+        public void IntEnumConvert_Should_Skip()
+        {
+            var myConverter = new IntEnumConverter();
+
+            var myObject = "Fake";
+            var Result = myConverter.Convert(myObject, null, null, null);
+            var Expected = 0;
+
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
+    }
+}
