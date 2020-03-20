9abf0af1aa3be7fb34bd86870a4cf069bdbb059e
Tianhua Zhao <tzhaojustdoit@gmail.com>
2020-03-19 22:51:57 -0700

    Added BattleEngine_Constructor_Default_Should_Pass

--- UnitTests/Engine/BattleEngineTests.cs
+++ UnitTests/Engine/BattleEngineTests.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using NUnit.Framework;
+using Game.Engine;
+using Game.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace UnitTests.Engine
 {
-    class BattleEngineTests
+    [TestFixture]
+    public class BattleEngineTests
     {
+        BattleEngine Engine;
+
+        [SetUp]
+        public void Setup()
+        {
+            Engine = new BattleEngine();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void BattleEngine_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = Engine;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
     }
 }
