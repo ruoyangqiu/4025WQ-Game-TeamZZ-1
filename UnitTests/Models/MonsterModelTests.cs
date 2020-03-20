94a2b7ccacd520c3f1db3f74fdc01ecfa8873540
Tianhua Zhao <tzhaojustdoit@gmail.com>
2020-03-19 23:20:12 -0700

    Added MonsterModel_Constructor_Default_Should_Pass

--- UnitTests/Models/MonsterModelTests.cs
+++ UnitTests/Models/MonsterModelTests.cs
@@ -1,10 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
+
+using Game.Models;
+using Game.ViewModels;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace UnitTests.Models
 {
-    class MonsterModelTests
+    [TestFixture]
+    public class MonsterModelTests
     {
+        [TearDown]
+        public void TearDown()
+        {
+            ItemIndexViewModel.Instance.Dataset.Clear();
+        }
+
+        [Test]
+        public void MonsterModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new MonsterModel();
+
+            // Reset
+
+            // Assert 
+            Assert.IsNotNull(result);
+        }
     }
 }
