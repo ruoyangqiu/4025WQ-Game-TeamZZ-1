4d1e2869c8e37b02cb6c11a96b98206c2403e349
Ruoyang Qiu <robinqiu1992@gmail.com>
2020-03-19 21:12:20 -0700

    Added ConvertFromJson to ItemModelJsonHelper

--- Game/Game/Helpers/ItemModelJsonHelper.cs
+++ Game/Game/Helpers/ItemModelJsonHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Game.Models;
+using Game.Services;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
@@ -50,6 +51,23 @@ namespace Game.Helpers
         public static ItemModel ConvertFromJson(JObject json)
         {
             var myData = new ItemModel();
+
+            myData.Name = JsonHelper.GetJsonString(json, "Name");
+            myData.Guid = JsonHelper.GetJsonString(json, "Guid");
+            myData.Id = myData.Guid;    // Set to be the same as Guid, does not come down from server, but needed for DB
+
+            myData.Description = JsonHelper.GetJsonString(json, "Description");
+            myData.ImageURI = JsonHelper.GetJsonString(json, "ImageURI");
+
+            myData.Value = JsonHelper.GetJsonInteger(json, "Value");
+            myData.Range = JsonHelper.GetJsonInteger(json, "Range");
+            myData.Damage = JsonHelper.GetJsonInteger(json, "Damage");
+
+            //myData.Category = JsonHelper.GetJsonInteger(json, "Category");
+
+            myData.Location = (ItemLocationEnum)JsonHelper.GetJsonInteger(json, "Location");
+            myData.Attribute = (AttributeEnum)JsonHelper.GetJsonInteger(json, "Attribute");
+
             return myData;
         }
 
