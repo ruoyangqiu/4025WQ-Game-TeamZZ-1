a8278bd421e317b1a33f6c9398758c5075074bd3
Tejas Dandane <tejudandane@gamil.com>
2020-03-19 23:51:34 -0700

    Added test StringEnumConvert_String_Should_Pass

--- UnitTests/Helpers/IntEnumConverterHelperTests.cs
+++ UnitTests/Helpers/IntEnumConverterHelperTests.cs
@@ -32,5 +32,17 @@ namespace UnitTests.Helpers
 
             Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
         }
+
+        [Test]
+        public void StringEnumConvert_String_Should_Pass()
+        {
+            var myConverter = new StringEnumConverter();
+
+            var myObject = "Feet";
+            var Result = myConverter.Convert(myObject, typeof(ItemLocationEnum), null, null);
+            var Expected = ItemLocationEnum.Feet;
+
+            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+        }
     }
 }
